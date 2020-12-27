b52282032abfb30a49983b73e489b7d70bbd0972
Roald Nefs <info@roaldnefs.com>
2020-12-27 21:58:03 +0100

    Initial commit
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def get_version() -> str:
+    with open("transip/__init__.py") as file:
+        for line in file:
+            if line.startswith("__version__"):
+                return line.replace("\"", "").split()[-1]
+
+
+def get_long_description() -> str:
+    with open("README.md", "r") as file:
+        return file.read()
+
+
+setup(
+    name="python-transip",
+    version=None,
+    description="Wrapper for the TransIP API",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Roald Nefs",
+    author_email="info@roaldnefs.com",
+    license="LGPLv3",
+    url="https://github.com/roaldnefs/python-transip",
+    install_requires=[],
+    python_requires=">=3.6.12",
+    entry_points={},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    extras_require={},
+)
---
+++ transip/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 Roald Nefs <info@roaldnefs.com>
+#
+# This file is part of python-transip.
+
+# python-transip is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+"""Wrapper for the TransIP API."""
+
+from transip.client import TransIP
+
+__title__ = "python-transip"
+__version__ = "0.0.1"
+__author__ = "Roald Nefs"
+__email__ = "info@roaldnefs.com"
+__copyright__ = "Copyright 2020, Roald Nefs"
+__license__ = "LGPL3"
