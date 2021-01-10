25821fb64990bb9456f3527f0d76c6b6399f7214
Roald Nefs <info@roaldnefs.com>
2021-01-10 21:12:37 +0100

    Bump version to 0.3.0
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- transip/__init__.py
+++ transip/__init__.py
@@ -29,7 +29,7 @@ from transip.utils import generate_message_signature, generate_nonce
 
 
 __title__ = "python-transip"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "Roald Nefs"
 __email__ = "info@roaldnefs.com"
 __copyright__ = "Copyright 2020-2021, Roald Nefs"
